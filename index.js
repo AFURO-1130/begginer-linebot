b929b595e615256c41c7c70e40ca2593b546b802
AFURO-1130 <email@example.com>
2021-02-10 22:19:21 +0900

    コメントつけた

--- index.js
+++ index.js
@@ -60,202 +60,201 @@ exports.handler = (event, context, callback) => {
         console.log("署名認証エラー");
     }
 };
-//問題のflexメッセーを入れるところ
+//問題のflexメッセージを入れるところ
 const quiz = [
-{
-    "type": "flex",
-    "altText": "問題だよ",
-    "contents":
     {
-        "type": "bubble",
-        "direction": "ltr",
-        "action": {
-            "type": "postback",
-            "label": "あああ",
-            
-            "data": "あああ"
-        },
-        "header": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                    "type": "text",
-                    "text": "Header",
-                    "align": "center",
-                    "contents": []
-                }
-            ]
-        },
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                    "type": "text",
-                    "text": "had betterに意味は？",
-                    "align": "center",
-                    "contents": []
-                }
-            ]
-        },
-        "footer": {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-                {
-                    "type": "button",
-                    "action": {
-                        "type": "postback",
-                        "label": "~する方が良い",
-                        
-                        "data": "0"
-                    },
-                    "gravity": "top"
-                },
-                {
-                    "type": "button",
-                    "action": {
-                        "type": "postback",
-                        "label": "〜した方がよい",
-                        
-                        "data": "1"
+        "type": "flex",
+        "altText": "問題だよ",
+        "contents":
+        {
+            "type": "bubble",
+            "direction": "ltr",
+            "action": {
+                "type": "postback",
+                "label": "あああ",
+
+                "data": "あああ"
+            },
+            "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "Header",
+                        "align": "center",
+                        "contents": []
                     }
-                }
-            ]
+                ]
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "had betterに意味は？",
+                        "align": "center",
+                        "contents": []
+                    }
+                ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
+                        "type": "button",
+                        "action": {
+                            "type": "postback",
+                            "label": "~する方が良い",
+                            "data": "0"
+                        },
+                        "gravity": "top"
+                    },
+                    {
+                        "type": "button",
+                        "action": {
+                            "type": "postback",
+                            "label": "〜した方がよい",
+
+                            "data": "1"
+                        }
+                    }
+                ]
+            }
         }
     }
-}
-,{
-  "type": "flex",
-  "altText": "問題だよーん",
-  "contents": {
-  "type": "bubble",
-  "direction": "ltr",
-  "action": {
-    "type": "postback",
-    "label": "あああ",
-    "data": "あああ"
-  },
-  "header": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Header",
-        "align": "center",
-        "contents": []
-      }
-    ]
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "2021!の末尾に0は何個並ぶでしょう？",
-        "align": "center",
-        "contents": []
-      }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "horizontal",
-    "contents": [
-      {
-        "type": "button",
-        "action": {
-          "type": "postback",
-          "label": "55",
-          "data": "3"
-        },
-        "gravity": "top"
-      },
-      {
-        "type": "button",
+    , {
+        "type": "flex",
+        "altText": "問題だよーん",
+        "contents": {
+            "type": "bubble",
+            "direction": "ltr",
+            "action": {
+                "type": "postback",
+                "label": "あああ",
+                "data": "あああ"
+            },
+            "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "Header",
+                        "align": "center",
+                        "contents": []
+                    }
+                ]
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "2021!の末尾に0は何個並ぶでしょう？",
+                        "align": "center",
+                        "contents": []
+                    }
+                ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
+                        "type": "button",
+                        "action": {
+                            "type": "postback",
+                            "label": "55",
+                            "data": "3"
+                        },
+                        "gravity": "top"
+                    },
+                    {
+                        "type": "button",
 
-        "action": {
-          "type": "postback",
-          "label": "50",
-          "data": "2"
+                        "action": {
+                            "type": "postback",
+                            "label": "50",
+                            "data": "2"
+                        }
+                    }
+                ]
+            }
         }
-      }
-    ]
-  }
-}
-},{
-  "type": "flex",
-  "altText": "問題だよーん",
-  "contents": {
-  "type": "bubble",
-  "direction": "ltr",
-  "action": {
-    "type": "postback",
-    "label": "あああ",
-    
-    "data": "あああ"
-  },
-  "header": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Header",
-        "align": "center",
-        "contents": []
-      }
-    ]
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "教習おわるのはいつか？？",
-        "align": "center",
-        "contents": []
-      }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "horizontal",
-    "contents": [
-      {
-        "type": "button",
-        "action": {
-          "type": "postback",
-          "label": "29日",
-          
-          "data": "4"
-        },
-        "gravity": "top"
-      },
-      {
-        "type": "button",
+    }, {
+        "type": "flex",
+        "altText": "問題だよーん",
+        "contents": {
+            "type": "bubble",
+            "direction": "ltr",
+            "action": {
+                "type": "postback",
+                "label": "あああ",
 
-        "action": {
-          "type": "postback",
-          "label": "30日",
-          
-          "data": "5"
+                "data": "あああ"
+            },
+            "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "Header",
+                        "align": "center",
+                        "contents": []
+                    }
+                ]
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "教習おわるのはいつか？？",
+                        "align": "center",
+                        "contents": []
+                    }
+                ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
+                        "type": "button",
+                        "action": {
+                            "type": "postback",
+                            "label": "29日",
+
+                            "data": "4"
+                        },
+                        "gravity": "top"
+                    },
+                    {
+                        "type": "button",
+
+                        "action": {
+                            "type": "postback",
+                            "label": "30日",
+
+                            "data": "5"
+                        }
+                    }
+                ]
+            }
         }
-      }
-    ]
-  }
-}
-}];
+    }];
 
-const judge =[{
-    type:"text",text:"正解だよ",
+const judge = [{
+    type: "text", text: "正解だよ",
 },
 {
-    type:"text",text:"もう一度考えよう！"
+    type: "text", text: "もう一度考えよう！"
 }];
 
 
@@ -263,87 +262,86 @@ let message;
 let point = 0;
 //テキストを送られたら返すメッセージ
 //このメッセージを送ると点数リセット
-const messageFunc = async function (event){
+const messageFunc = async function (event) {
     message = quiz[0];
     point = 0;
     return message;
-    };
-    //続きの問題
-    
+};
+//続きの問題
 const postbackFunc = async function (event) {
-  const result = {
-    "type": "flex",
-    "altText": "結果発表",
-    "contents": {
-      "type": "bubble",
-      "direction": "ltr",
-      "action": {
-        "type": "postback",
-        "label": "あああ",
-        "text": "ああ",
-        "data": "あああ"
-      },
-      "header": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "Header",
-            "align": "center",
-            "contents": []
-          }
-        ]
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "結果発表",
-            "align": "center",
-            "contents": []
-          }
-        ]
-      },
-      "footer": {
-        "type": "box",
-        "layout": "horizontal",
-        "contents": [
-          {
-            "type": "text",
-            "text": "スコアは" + point +"点です",
-            "align": "center",
-            "contents": []
-          }
-        ]
-      }
-    }
-  };
-    if(event.postback.data === "1"){
-      ++point;
-      point++;
-        message = [judge[0],quiz[1]];
-     return message;
-    }
-    else if(event.postback.data === "3"){
+    const result = {
+        "type": "flex",
+        "altText": "結果発表",
+        "contents": {
+            "type": "bubble",
+            "direction": "ltr",
+            "action": {
+                "type": "postback",
+                "label": "あああ",
+                "text": "ああ",
+                "data": "あああ"
+            },
+            "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "Header",
+                        "align": "center",
+                        "contents": []
+                    }
+                ]
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "結果発表",
+                        "align": "center",
+                        "contents": []
+                    }
+                ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "スコアは" + point + "点です",
+                        "align": "center",
+                        "contents": []
+                    }
+                ]
+            }
+        }
+    };
+    //正誤判定
+    if (event.postback.data === "1") {
+        ++point;
         point++;
-        message = [judge[0],quiz[2]];
-        
+        message = [judge[0], quiz[1]];
         return message;
     }
-    else if(event.postback.data === "5"){
+    else if (event.postback.data === "3") {
         point++;
-        message = [judge[0],result];
+        message = [judge[0], quiz[2]];
+
         return message;
     }
-    else{
+    else if (event.postback.data === "5") {
+        point++;
+        message = [judge[0], result];
+        return message;
+    }
+    else {
         message = judge[1];
         point--;
         return message;
     }
-  };
-    
-    
-  
+};
+
+
